8af9d26d9706558dfbe673f2fc5b7e870bd0a528
inxeoz <inxeoz@users.noreply.github.com>
2024-01-03 21:08:47 +0530

    rust programs

---
+++ main.rs
@@ -0,0 +1,9 @@
+
+mod get_user_integer;
+fn main(){
+    get_user_integer::run();
+    // let  mut input = String::new();
+    // stdin().read_line(&mut input).unwrap();
+    // let n = input.trim().parse::<i32>().unwrap();
+    // println!("{n}");
+}
