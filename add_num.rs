86149107ebbd6db180d9f4c9cb985d89ac18786c
Purushottam Singram <87688500+inxeoz@users.noreply.github.com>
2024-01-21 22:19:34 +0530

    added encoder

--- add_num.rs
+++
@@ -1,7 +0,0 @@
-
-use std;
-pub fn add_num(a:i32, b:i32 ){
-    let c = a+b;
-    print!("{c}");
-
-}
--- main.rs
+++
@@ -1,8 +0,0 @@
-
-mod get_user_integer;
-mod add_num;
-
-
-fn main(){
-   // get_user_integer::run();
-}
